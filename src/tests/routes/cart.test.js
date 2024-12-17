e38c7ae98ff492cf0a10ab24150ef6837745cfa3
Mel Nielsen <mel29nielsen@gmail.com>
2024-12-16 20:08:16 -0500

    Update Test

--- src/tests/routes/cart.test.js
+++ src/tests/routes/cart.test.js
@@ -6,12 +6,16 @@ const cartRouter = require('../../routes/cart');
 const Cart = require('../../models/cart');
 const Product = require('../../models/product');
 const Category = require('../../models/category');
+const CartItems = require('../../models/cartItem');
 
 
 const app = express();
 app.use(bodyParser.json());
 app.use('/api/carts', cartRouter);
 
+
+let userId = 45;
+let carrito, categoria, producto, items;
 
 describe('Cart Routes', () => {
   beforeAll(async () => {
@@ -26,6 +30,20 @@ describe('Cart Routes', () => {
     await Cart.destroy({ where: {} });
     await Product.destroy({ where: {} });
     await Category.destroy({ where: {} });
+
+    categoria = await Category.create({
+      name: "Perro Categoria",
+      description: "Diabluuuuuuuu el pegggrroo"
+    });
+
+    producto =  await Product.create({
+      name: "Moco de Mono",
+      price: 3.60,
+      inventory: 3,
+      categoryId: categoria.id
+    });
+
+    carrito = await Cart.create({userId: userId});
   });
 
   describe('POST /api/carts/:userId', () => {
@@ -52,24 +70,24 @@ describe('Cart Routes', () => {
     });
 
     //USUARIO NULL
-    it('no deberia crear el carrito', async () => {
-      const cartData = {
-        userId: null,
-      }
-
-      const response = await request(app)
-          .post(`/api/carts/${cartData.userId}`)
-          .expect(400)
-
-      //console.log(response.body);
-      expect(response.body).toHaveProperty('error');
-
-      //Base de datos
-
-      const cart = await Cart.findByPk(response.body.id);
-      //console.log(cart)
-      expect(cart).not.toBeNull();
-    });
+    // it('no deberia crear el carrito', async () => {
+    //   const cartData = {
+    //     userId: null,
+    //   }
+    //
+    //   const response = await request(app)
+    //       .post(`/api/carts/${cartData.userId}`)
+    //       .expect(400)
+    //
+    //   //console.log(response.body);
+    //   expect(response.body).toHaveProperty('error');
+    //
+    //   //Base de datos
+    //
+    //   const cart = await Cart.findByPk(response.body.id);
+    //   //console.log(cart)
+    //   expect(cart).not.toBeNull();
+    // });
 
 
   });
@@ -87,38 +105,135 @@ describe('Cart Routes', () => {
     //   });
     //   cart = await Cart.create({ userId: '1' });
     // });
-    let userId = 45;
-    let carrito, categoria, producto;
-
-    beforeEach( async () =>{
-
-      categoria = await Category.create({
-        name: "Perro Categoria",
-        description: "Diabluuuuuuuu el pegggrroo"
-      });
-
-      producto =  await Product.create({
-        name: "",
-        price: 0.0,
-        categoryId: categoria.id
-      });
-
-      carrito = await Cart.create({userId: userId});
-
-
-    });
 
     it('should add item to cart', async () => {
 
+      const params = {
+        productId: producto.id,
+        quantity: 3
+      };
+
+      const response = await request(app)
+          .post(`/api/carts/${carrito.id}/items`)
+          .send(params)
+          .expect(201);
+      console.log(response.body)
+
+      expect(response.body).toHaveProperty('id');
+      expect(response.body).toHaveProperty('productId', producto.id);
+      expect(response.body).toHaveProperty('quantity', params.quantity);
 
 
     });
+
+    // NO EXISTE INVENTARIO
+    it('should not add item to cart NO INVENTORY', async () => {
+
+      const params = {
+        productId: producto.id,
+        quantity: 10
+      };
+
+      const response = await request(app)
+          .post(`/api/carts/${carrito.id}/items`)
+          .send(params)
+          .expect(400);
+      //console.log(response)
+
+      expect(response.body).toHaveProperty('error', 'Not enough inventory available');
+
+    });
+
+    //NO EXISTE EL ITEM
+    it('should not add item to cart NO EXISTE ITEM', async () => {
+
+      const params = {
+        productId: 12,//producto.id,
+        quantity: 10
+      };
+
+      const response = await request(app)
+          .post(`/api/carts/${carrito.id}/items`)
+          .send(params)
+          .expect(400);
+      //console.log(response)
+
+      expect(response.body).toHaveProperty('error', 'Product not found');
+
+    });
+
 
   });
 
   describe('GET /api/carts/:cartId/items', () => {
-    it('should return cart items with totals', async () => {
+
+    beforeEach({
 
     });
+
+    //LISTAR LOS ITEMS
+    it('should return cart items with totals', async () => {
+
+      const response = await  request(app)
+          .get(`/api/carts/${carrito.id}/items`)
+          .expect(200)
+
+      console.log("TEST TOTAL ITEMS",response.body)
+
+      expect(response.body).toHaveProperty('items')
+      expect(response.body).toHaveProperty('summary')
+
+    });
+
+
+    // it('should no return cart items with totals', async () => {
+    //
+    //   const response = await  request(app)
+    //       .get(`/api/carts/ww2/items`)
+    //       .expect(200)
+    //
+    //   console.log(response.body)
+    //
+    //   expect(response.body).toHaveProperty('items')
+    //   expect(response.body).toHaveProperty('summary')
+    //
+    // });
   });
+
+
+  describe('PUT /cart/:cartId/items/:itemId', () => {
+
+    //ACTUALIZAR CANTIDAD DE LOS ITEMS
+    it('should Updates the quantity of an item in the cart', async () => {
+
+      console.log(carrito);
+      const items = await CartItems.findAll();
+
+      console.log(items);
+
+      const response = await  request(app)
+          .get(`/api/carts/${carrito.id}/items/${items[0].id}`)
+          .expect(200);
+
+      expect(response.body).toHaveProperty('items')
+      expect(response.body).toHaveProperty('summary')
+
+    });
+
+
+    // it('should no return cart items with totals', async () => {
+    //
+    //   const response = await  request(app)
+    //       .get(`/api/carts/ww2/items`)
+    //       .expect(200)
+    //
+    //   console.log(response.body)
+    //
+    //   expect(response.body).toHaveProperty('items')
+    //   expect(response.body).toHaveProperty('summary')
+    //
+    // });
+  });
+
+
 });
